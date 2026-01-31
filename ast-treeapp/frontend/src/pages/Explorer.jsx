d9a42122981c30c5167f8df3be62baff89351971
DanielPetk <danielpetkoski@hotmail.com>
2026-01-31 18:01:16 -0500

    small change

--- ast-treeapp/frontend/src/pages/Explorer.jsx
+++ ast-treeapp/frontend/src/pages/Explorer.jsx
@@ -125,7 +125,7 @@ export default function Explorer() {
           onChange={setGrammarRules}
           help={
             <>
-              PEG style expressions.
+              EBNF style expressions.
             </>
           }
         />
